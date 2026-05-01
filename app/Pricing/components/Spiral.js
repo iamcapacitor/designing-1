026529abdeaa8d888276b8c03a4c97045099c8b5
Capacitor <rohitkhengare172@gmail.com>
2026-05-01 12:44:37 +0530

    spiral completed oddly

---
+++ app/Pricing/components/Spiral.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "@/app/Pricing/components/spiral.css";
+const SpiralComponent = () => {
+  return (
+    <div className='spiralcontainer h-screen bg-cyan-100 relative'>
+      <div className="spiralitem brightness-[1] border-2"><img src="/other/image1.png" alt="image1" /></div>
+    </div>
+  )
+}
+
+export default SpiralComponent
